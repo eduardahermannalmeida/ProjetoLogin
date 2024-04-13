c9eca709c6ff2b488375b349b5e1035852a0c149
eduardahermannalmeida <dudahdealmeida29@gmail.com>
2024-04-13 10:42:39 -0300

    ProjetoLogin

---
+++ src/br/ulbra/entity/Usuario.java
@@ -0,0 +1,57 @@
+
+package br.ulbra.entity;
+
+
+public class Usuario {
+    
+    private int pkIdUsu;
+    private String nomeUsu;
+    private String emailUsu;
+    private String senhaUsu;
+    
+    public Usuario(){
+        this.pkIdUsu = 1;
+        this.nomeUsu = "Xiru da Silva";
+        this.emailUsu = "x@x.com";
+        this.senhaUsu = "123";
+    }
+
+    public int getPkIdUsu() {
+        return pkIdUsu;
+    }
+
+    public void setPkIdUsu(int pkIdUsu) {
+        this.pkIdUsu = pkIdUsu;
+    }
+
+    public String getNomeUsu() {
+        return nomeUsu;
+    }
+
+    public void setNomeUsu(String nomeUsu) {
+        this.nomeUsu = nomeUsu;
+    }
+
+    public String getEmailUsu() {
+        return emailUsu;
+    }
+
+    public void setEmailUsu(String emailUsu) {
+        this.emailUsu = emailUsu;
+    }
+
+    public String getSenhaUsu() {
+        return senhaUsu;
+    }
+
+    public void setSenhaUsu(String senhaUsu) {
+        this.senhaUsu = senhaUsu;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "pkIdUsu=" + pkIdUsu + ", nomeUsu=" + nomeUsu + ", emailUsu=" + emailUsu + ", senhaUsu=" + senhaUsu + '}';
+    }
+    
+    
+}
